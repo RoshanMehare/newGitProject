f58dd8071370589e342921a75e8db6b42f3353aa
Roshan mehare <roshanmehare17@gmail.com>
2022-10-13 17:24:37 +0530

    first commit is done

---
+++ ArrayListDemo/src/com/demo/ArrayList.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		List<String>list=Arrays.asList("Roshan","Nikhil","Suresh","Deepak");
+		System.out.println(list); 
+	}
+
+}
